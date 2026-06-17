583b402ecef0dfadb339601604e15138fd00ff36
0x6dflux <mohammadi.mahdi.mecheng@gmail.com>
2026-06-17 14:42:20 +0330

    Add CartModel

---
+++ sale/models/cart_model.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+from general.models import BaseModel
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class CartModel(BaseModel):
+    user_id = models.ForeignKey(User, )
+    total_amount = models.DecimalField(max_digits=10, decimal_places=1)
