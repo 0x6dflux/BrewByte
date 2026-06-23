afed000e4985ecea6d735fb965e0c0a31700f793
MahdiMohammadiME <mohammadi.mahdi.me@gmail.com>
2026-06-24 00:00:21 +0330

    Update BaseModel
    Two new fields, `created_by` and `updated_by`, have been added to track
    the user who made changes.

--- general/models/base_model.py
+++ general/models/base_model.py
@@ -1,7 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
+AUTH_USER = get_user_model()
 
-# Create your models here.
+
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(AUTH_USER, models.CASCADE)
+    updated_by = models.ForeignKey(AUTH_USER, models.CASCADE, null=True, blank=True)
