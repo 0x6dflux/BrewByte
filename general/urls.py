2ecf2bb53d00d523c79e3431ec0eb2b10bdd4ae0
sepehremami <sepehr7890@gmail.com>
2026-06-24 23:09:28 +0200

    add cart/update/ view

--- general/urls.py
+++ general/urls.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 from django.urls import path, include
 from general.views import HomeView
-
+from general.views import add_to_cart
 app_name = 'general'
 urlpatterns = [
     path("", HomeView.as_view(), name="home" ),
+    path("cart/update/", add_to_cart),
 
 ]
--- general/views/home_view.py
+++ general/views/home_view.py
@@ -1,46 +1,49 @@
+import json
+
 from django.shortcuts import render, redirect
 from django.views import View
-# from sale.models import CartModel, CartItemModel
-# from inventory.models import Product, Category
+from django.http import JsonResponse, HttpResponseNotAllowed
+from sale.models import CartItemModel, CartModel
+from inventory.models import Product, Category
 
-class Category:
-    def __init__(self,name):
-        self.name = name
+# class Category:
+#     def __init__(self,name):
+#         self.name = name
 
 
-class Product:
-    def __init__(self,id,name,price,ingredients, sale_stock, category):
-        self.id = id
-        self.name = name
-        self.price = price
-        self.ingredients = ingredients
-        self.sale_stock = sale_stock
-        self.category = category
+# class Product:
+#     def __init__(self,id,name,price,ingredients, sale_stock, category):
+#         self.id = id
+#         self.name = name
+#         self.price = price
+#         self.ingredients = ingredients
+#         self.sale_stock = sale_stock
+#         self.category = category
 
-class CartItemModel:
-    def __init__(self,product_id,cart_id,price,quantity,amount):
-        self.product_id = product_id
-        self.cart_id = cart_id
-        self.price = price
-        self.quantity = quantity
-        self.amount = amount 
+# class CartItemModel:
+#     def __init__(self,product_id,cart_id,price,quantity,amount):
+#         self.product_id = product_id
+#         self.cart_id = cart_id
+#         self.price = price
+#         self.quantity = quantity
+#         self.amount = amount 
 
-class CartModel:
-    def __init__(self,user_id,total_amount):
-        self.user_id = user_id
-        self.total_amount = total_amount
+# class CartModel:
+#     def __init__(self,user_id,total_amount):
+#         self.user_id = user_id
+#         self.total_amount = total_amount
 
 
 class HomeView(View):
     template_name = "general/home.html"
     def get(self,request):
-        # categories = Category.objects.all()
-        # products = Product.objects.all()
-        categories = [Category("Drink"), Category("Food")]
-        products = [Product(1,"lemon soda",50, "water,lemon", 5, "Drink"),
-                    Product(2,"ginger soda",50, "water,ginger", 10, "Drink"),
-                    Product(3,"Burger",200, "200g meat, pickle, tomato, bun", 2, "Food"),
-                    Product(4,"Cheese Burger",250, "200g meat,goda cheese, pickle, tomato, bun", 2, "Food"),]
+        categories = Category.objects.all()
+        products = Product.objects.all()
+        # categories = [Category("Drink"), Category("Food")]
+        # products = [Product(1,"lemon soda",50, "water,lemon", 5, "Drink"),
+        #             Product(2,"ginger soda",50, "water,ginger", 10, "Drink"),
+        #             Product(3,"Burger",200, "200g meat, pickle, tomato, bun", 2, "Food"),
+        #             Product(4,"Cheese Burger",250, "200g meat,goda cheese, pickle, tomato, bun", 2, "Food"),]
         return render(request, self.template_name, context={"categories":categories, "products":products} )
     
     # def post(self,request):
@@ -49,4 +52,26 @@ class HomeView(View):
     #     for id, quantity in products.items():
             
     #         cart_item = CartItemModel.objects.create(id , )
+
+def add_to_cart(request):
+    print(request.user)
+    if request.method != 'POST':
+        return HttpResponseNotAllowed('not allowed')
     
+    res = json.loads(request.body)
+    # print(res)
+    product_id = res.get('product_id')
+    quantity = res.get('quantity')
+
+    try:
+        cart = CartModel.objects.get(user_id=request.user.id)
+    except:
+        cart = CartModel.objects.create(user_id=request.user, total_amount=0)
+
+    product = Product.objects.get(id=int(product_id))
+    CartItemModel
+    print(cart)
+
+    # print(dir(request))
+
+    return JsonResponse(data={'res': 'ok.'})
