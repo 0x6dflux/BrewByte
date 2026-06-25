b36ffb537b6cb53d2049c4c319127256505a7341
MahdiMohammadiME <mohammadi.mahdi.me@gmail.com>
2026-06-25 18:16:08 +0330

    Modify CartItemModel and OrderItemModel fields

--- sale/models/cart.py
+++ sale/models/cart.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.contrib.auth import get_user_model
 from django.db import models
 
@@ -9,15 +11,24 @@ AUTH_USER = get_user_model()
 
 class CartModel(BaseModel):
     user_id = models.ForeignKey(AUTH_USER, models.CASCADE)
-    total_amount = models.DecimalField(max_digits=10, decimal_places=1)
+    total_amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=1,
+        default=Decimal(0.0),
+    )
 
     def __str__(self) -> str:
         return f"CR{self.pk}-{self.user_id.first_name}-{self.user_id.last_name}"
 
 
 class CartItemModel(BaseModel):
-    product_id = models.ForeignKey(Product, models.CASCADE)
-    cart_id = models.ForeignKey(CartModel, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, models.CASCADE, null=True, blank=True)
+    cart_id = models.ForeignKey(
+        CartModel,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
     price = models.DecimalField(max_digits=10, decimal_places=1)
     quantity = models.IntegerField()
     amount = models.DecimalField(max_digits=10, decimal_places=1)  # price * quantity
--- sale/models/order.py
+++ sale/models/order.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.contrib.auth import get_user_model
 from django.db import models
 
@@ -14,17 +16,25 @@ class OrderModel(BaseModel):
     user_id = models.ForeignKey(AUTH_USER, models.CASCADE)
     address_id = models.ForeignKey(Address, models.CASCADE)
     discount_id = models.ForeignKey(DiscountModel, models.CASCADE)
-    total_amount = models.DecimalField(max_digits=10, decimal_places=1)
+    total_amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=1,
+        default=Decimal(0.0),
+    )
     tax = models.DecimalField(max_digits=10, decimal_places=1)
-    taxed_amount = models.DecimalField(max_digits=10, decimal_places=1)
+    taxed_amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=1,
+        default=Decimal(0.0),
+    )
 
     def __str__(self) -> str:
         return f"OR{self.pk}-{self.user_id.first_name}-{self.user_id.last_name}"
 
 
 class OrderItemModel(BaseModel):
-    order_id = models.ForeignKey(OrderModel, models.CASCADE)
-    product_id = models.ForeignKey(Product, models.CASCADE)
+    order_id = models.ForeignKey(OrderModel, models.CASCADE, null=True, blank=True)
+    product_id = models.ForeignKey(Product, models.CASCADE, null=True, blank=True)
     quantity = models.DecimalField(max_digits=10, decimal_places=1)
     price = models.DecimalField(max_digits=10, decimal_places=1)
     amount = models.DecimalField(max_digits=10, decimal_places=1)
