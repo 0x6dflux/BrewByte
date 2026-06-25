e7ccb363b7e45fd859d4f7e9d94b1e27c85a84e6
MahdiMohammadiME <mohammadi.mahdi.me@gmail.com>
2026-06-25 17:37:35 +0330

    Add __str__ for activity app models

--- activity/models/favorite_model.py
+++ activity/models/favorite_model.py
@@ -10,3 +10,6 @@ AUTH_USER = get_user_model()
 class FavoriteModel(BaseModel):
     user_id = models.ForeignKey(AUTH_USER, models.CASCADE)
     product_id = models.ForeignKey(Product, models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"FV{self.pk}-{self.product_id.name}-{self.user_id.first_name}-{self.user_id.last_name}"
--- activity/models/notification_model.py
+++ activity/models/notification_model.py
@@ -9,8 +9,15 @@ AUTH_USER = get_user_model()
 
 
 class NotificationModel(BaseModel):
-    responsible_user_id = models.ForeignKey(AUTH_USER, models.CASCADE, related_name="notification_responsible_user_id")
+    responsible_user_id = models.ForeignKey(
+        AUTH_USER,
+        models.CASCADE,
+        related_name="notification_responsible_user_id",
+    )
     order_id = models.ForeignKey(OrderModel, models.CASCADE, null=True, blank=True)
     comment_id = models.ForeignKey(CommentModel, models.CASCADE, null=True, blank=True)
     description = models.TextField()
     is_seen = models.BooleanField()
+
+    def __str__(self) -> str:
+        return f"NT{self.pk}-{self.responsible_user_id.first_name}-{self.responsible_user_id.last_name}"
