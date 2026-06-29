0daa525b74bd032d22933e8c9622aa6938ce637e
amahmadi9877-hub <am.ahmadi9877@gmail.com>
2026-06-30 00:08:49 +0330

    Initiaialize Order url and view 2

--- sale/views/create_order.py
+++ sale/views/create_order.py
@@ -1,27 +1,30 @@
+from django.views import View
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
 from sale.models import OrderModel, OrderItemModel, CartModel, CartItemModel
 from client.models import User
 from general.models import BusinessSetting
+from sale.utils import get_tax_ratio
 
 @login_required
-class CreateOrder(request):
+class CreateOrder(View):
     template_name = 'client/signup.html'
-    tax_ratio = BusinessSetting.objects.first().default_tax_ratio
+    tax_ratio = get_tax_ratio()
     def get(self, request):
-        target_user = request.user
+        user = request.user
         try:
-            active_order  = OrderModel.objects.get(user_id = target_user, is_active = True)
-            active_order_items = OrderItemModel.objects.filter(order_id = active_order)
-            return render(
-                        request,
-                        self.template_name,
-                        content= {"active_order": active_order ,
-                                    "active_order_items":active_order_items })
-        except OrderModel.DoesNotExist:
-            try:
-                target_cart = CartModel.objects.get(user_id = target_user, is_active = True)
+            order  = OrderModel.objects.filter(user_id = user, is_active = True).first()
+            if order:
+                order_items = OrderItemModel.objects.filter(order_id = order)
+                if order_items:
+                    return render(
+                                request,
+                                self.template_name,
+                                context= {"active_order": active_order ,
+                                            "active_order_items":active_order_items})
+            else:
+                cart = CartModel.objects.filter(user_id = target_user, is_active = True)
                 target_cart_items = CartItemModel.objects.filter(cart_id = target_cart)
                 if len(target_cart_items) != 0:
                     active_order = OrderModel.objects.create(
@@ -33,6 +36,7 @@ class CreateOrder(request):
                         tax = target_cart.total_amount * (self.tax_ratio / 100 ),
                         taxed_amount = target_cart.total_amount,
                     )
+                    active_order_items = []
                     for cart_item in target_cart_items:
                         order_item = OrderItemModel.objects.create(
                             order_id = active_order,
@@ -41,4 +45,27 @@ class CreateOrder(request):
                             price = cart_item.price,
                             amount = cart_item.amount
                         )
+                        active_order_items.append(order_item)
+                    return render(
+                        request,
+                        self.template_name,
+                        context= {"active_order": active_order ,
+                                    "active_order_items":active_order_items })
+                else:
+                    error = "No Items In Your Cart!"
+                    return render(
+                        request,
+                        self.template_name,
+                        context= {"error": error })
+            except CartModel.DoesNotExist:
+                error = "No Cart Found!"
+                return render(
+                    request,
+                    self.template_name,
+                    context= {"error": error })
+
+    
+
+
+
             
