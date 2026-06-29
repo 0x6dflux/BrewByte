c4fc8ce27efb0a4035448ace5d95cfbd00d0a7f6
MahdiMohammadiME <mohammadi.mahdi.me@gmail.com>
2026-06-29 16:06:55 +0330

    Add/remove a product to/from favorites
    Users are able to add a product to their favorites, as well as removing
    from their favorites. This feature is done without refreshing the page.

--- general/views/home_view.py
+++ general/views/home_view.py
@@ -4,9 +4,8 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.views import View
 
+from activity.models import FavoriteModel
 from inventory.models import Category, Product
-
-# from activity.models import FavoriteModel
 
 # from sale.models import CartItemModel, CartModel
 
@@ -34,12 +33,54 @@ class HomeView(View):
         )
 
     def post(self, request):
-        print(request.body)
         my_body = json.loads(request.body)
-        print(my_body)
-        product_id = my_body.get("product_id")
-        product_id_value = my_body.get("value")
-        print(product_id)
-        print(product_id_value)
 
-        return HttpResponse(f"{product_id_value}")
+        js_request = my_body.get("request")
+        if js_request:
+            if request.user.id:
+                return HttpResponse("OK")
+            return HttpResponse("To add a product in your favorites, please sign in.")
+
+        star_product_id = my_body.get("star_product_id")
+        if star_product_id and (current_user := request.user):
+            user_favorite_products = Product.objects.filter(
+                favoritemodel__user_id=current_user.id
+            )
+            if Product.objects.get(id=star_product_id) in user_favorite_products:
+                FavoriteModel.objects.get(
+                    user_id=current_user.id,
+                    product_id=star_product_id,
+                ).delete()
+                return HttpResponse("✩")
+            else:
+                FavoriteModel(
+                    user_id=current_user,
+                    product_id=Product.objects.get(id=star_product_id),
+                ).save()
+                return HttpResponse("🌟")
+
+        js_request = my_body.get("update")
+        if js_request == "favorites":
+            user_favorite_products = Product.objects.filter(
+                favoritemodel__user_id=request.user.id
+            )
+
+            return render(
+                request,
+                "general/favorites.html",
+                context={
+                    "user_favorite_products": user_favorite_products,
+                },
+            )
+
+        product_id = my_body.get("product_id")
+        if product_id:
+            product_id_value = my_body.get("value")  # product_id is an int object
+
+            return HttpResponse(f"{product_id_value}")
+
+            # return render(
+            #     request,
+            #     "general/change_favorites.html",
+            #     context={"user_favorite_products": user_favorite_products},
+            # )
